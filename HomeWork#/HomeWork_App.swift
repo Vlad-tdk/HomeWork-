0599f5e43d0ac061162113ee67f41aca9b5e953b
Vladimir <xalif80@gmail.com>
2023-12-14 18:43:40 +0100

    Initial Commit

---
+++ HomeWork#/HomeWork_App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork_App.swift
+//  HomeWork#
+//
+//  Created by Vlad on 14.12.23..
+//
+
+import SwiftUI
+
+@main
+struct HomeWork_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
